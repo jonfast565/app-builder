d788e840ca07a8d53fc4006b584b68fba3f7286e
Jon Fast <jnfstdj656@gmail.com>
2021-03-31 15:27:14 -0400

    fixing stuff,b ut excel still doesn't work for this

--- src/excelbuilder.rs
+++ src/excelbuilder.rs
@@ -1,20 +1,53 @@
-use std::io;
+use calamine::{open_workbook_auto, Reader, Xlsx, DataType};
+use crate::models::RowDocument;
 
-pub struct ExcelDocument {
-    pub name: String,
-    pub header: Vec<String>,
-    pub first_row: Vec<String>,
-    pub document: Vec<Vec<String>>,
-}
+pub fn get_excel(filename: String, first_row_only: bool) -> RowDocument {
+    let mut excel = open_workbook_auto(filename.clone()).expect("Cannot open file");
+    
+    // default to the first worksheet for reading.
+    if let Some(Ok(r)) = excel.worksheet_range_at(0) {
+        let mut counter = 0;
+        let mut header = Vec::<String>::new();
+        let mut rows = Vec::<Vec<String>>::new();
 
-/*
-pub fn get_excel(    
-    delimiter: u8,
-    rdr: R,
-    filename: String,
-    first_row_only: bool) -> ExcelDocument {
-    ExcelDocument {
+        for row in r.rows() {
+            let mut column_vec = Vec::<String>::new();
+            for (_, c) in row.iter().enumerate() {
 
+                let value : String = match *c {
+                    DataType::Empty => String::from(""),
+                    DataType::String(ref s) => format!("{}", s),
+                    DataType::Float(ref f) | DataType::DateTime(ref f) => format!("{}", f),
+                    DataType::Int(ref i) => format!("{}", i),
+                    DataType::Error(ref e) => format!("{:?}", e),
+                    DataType::Bool(ref b) => format!("{}", b),
+                };
+
+                if counter == 0 {
+                    header.push(value.to_string());
+                } else {
+                    column_vec.push(value.to_string());
+                }
+            }
+
+            if counter != 0 {
+                rows.push(column_vec);
+            }
+    
+            if first_row_only && counter == 1 {
+                break;
+            }
+
+            counter += 1;
+        }
+
+        return RowDocument {
+            name: filename,
+            header: header,
+            first_row: rows[0].clone(),
+            document: rows
+        }
     }
+
+    panic!("Default sheet not found, document unable to be parsed");
 }
-*/
